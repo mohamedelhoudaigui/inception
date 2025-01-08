0cc396f6c94d177c7ff0756aaeba609eec992cc6
Mohamed El Houdaigui <mel-houd@e1r1p1.1337.ma>
2025-01-08 13:31:51 +0100

    zeb

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-sleep 7
+echo 'moved the config'
+mv /srcs/wp-config.php /var/www/html/wp-config.php
+
+sleep 10
 
 wp --allow-root config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST}
 
@@ -9,3 +12,4 @@ echo 'php-fpm starting ...'
 php-fpm7.4 -R -F -y /srcs/php-fpm.conf
 
 echo 'php-fpm failed'
+
