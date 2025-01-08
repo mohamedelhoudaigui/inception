021f193626e5fcb346e0dd95b21d37b47caaba28
mohamedelhoudaigui <mohamedelhoudaigui27@gmail.com>
2025-01-08 18:07:29 +0000

    invalid email in wordpress

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
 
-mysqld &
+service mariadb start
+sleep 5
 
-sleep 3
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO \`${DB_USER}\`@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
 
-# Check if database exists
-DB_EXISTS=$(mysql -e "SHOW DATABASES LIKE '${DB_NAME}';" | grep ${DB_NAME})
-
-if [ -z "$DB_EXISTS" ]; then
-    mysql <<EOF
-    CREATE DATABASE ${DB_NAME};
-    CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-    GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-    FLUSH PRIVILEGES;
-EOF
-fi
-
-mysqladmin shutdown
-
-echo 'staring mariadb...'
-
-mysqld
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
