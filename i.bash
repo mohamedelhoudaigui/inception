14464e0b39d02a39a2e0d60f06dd4af0aebbac17
Mohamed El Houdaigui <mohamedelhoudaigui27@gmail.com>
2025-03-11 15:55:49 +0000

    latest

--- i.bash
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl is-active docker
